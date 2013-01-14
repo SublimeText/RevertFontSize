bdb4bf9d3ebb9d23973ab581fff68fc2ddfd0189
Jeffrey Sadeli <jeffrey@sadeli.com>
2013-01-15 03:19:18 +0900

    Changed plugin settings behavior to utilize Sublime Text's built-in user preferences.

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 Helper plugin to quickly revert user's `font_size` value to a 'default'. Can be executed via keyboard shortcut `Ctrl+0` (or `Super+0` for OSX).
 
-Configure `RevertFontSize` settings in `Preferences >> Package Settings >> RevertFontSize >> Settings - User`.
+To start using `RevertFontSize` package, configure it by adding the following keys to your user preferences (`Preferences >> Settings - User`).
 
 ## Settings
-`default_font_size`:
 
-Specify your default font size value here. The plugin will then update Sublime Text's current font size to the value specified here on execution (e.g. via `Ctrl+0` or `Super+0`).
+#### `revert_font_size`
+
+Specify your font size value to revert here. The plugin will then update Sublime Text's current font size to the value specified here on execution.
+
+Example `Preferences.sublime-settings` file:
+```json
+{
+    "revert_font_size": 12.0
+}
+```
--- revert_font_size.py
+++ revert_font_size.py
@@ -2,15 +2,14 @@ import sublime
 import sublime_plugin
 
 class RevertFontSizeCommand(sublime_plugin.ApplicationCommand):
-    """Plugin to revert user 'font_size' value.
+    """Plugin to revert user 'font_size' to a 'default' value.
 
     Based on code by bizoo
     http://www.sublimetext.com/forum/viewtopic.php?f=3&t=9508
     """
     def run(self):
-        user_settings = sublime.load_settings('RevertFontSize.sublime-settings')
-        default_font_size = user_settings.get('default_font_size')
-
-        pref_settings = sublime.load_settings('Preferences.sublime-settings')
-        pref_settings.set('font_size', default_font_size)
-        sublime.save_settings('Preferences.sublime-settings')
+        preferences = sublime.load_settings("Preferences.sublime-settings")
+        revert_font_size = preferences.get("revert_font_size", 10)    # value 10 is the fallback, just in case
+        
+        preferences.set("font_size", revert_font_size)
+        sublime.save_settings("Preferences.sublime-settings")
