b8f3b425ade6b25dc7fc16071d0b6e6d1443ac07
Jeffrey Sadeli <jeffrey@sadeli.com>
2013-04-02 18:49:32 +0900

    Completed feature request support for initial font_size value discovery (note: for Sublime Text 3). Fixes #2.

--- revert_font_size.py
+++ revert_font_size.py
@@ -1,6 +1,25 @@
 import sublime
 import sublime_plugin
 
+def is_sublime_text_3():
+    """Returns True if this plugin is currently being run in Sublime Text 3."""
+    try:
+        return int(sublime.version()) >= 3000
+    except ValueError:
+        return sys.hexversion >= 0x030000F0
+
+def plugin_loaded():
+    """Called when this plugin is loaded; Sublime Text 3 API."""
+    preferences = sublime.load_settings("Preferences.sublime-settings")
+    if not preferences.get("revert_font_size"):
+        default_font_size = preferences.get("font_size", 10)
+        preferences.set("revert_font_size", default_font_size)
+        sublime.save_settings("Preferences.sublime-settings")
+
+def plugin_unloaded():
+    """Called when this plugin is unloaded; Sublime Text 3 API."""
+    pass
+
 class RevertFontSizeCommand(sublime_plugin.ApplicationCommand):
     """Plugin to revert user 'font_size' to a 'default' value.
 
